63dbaf192644ad5b801e6e9b3e997e42df60b182
Smile Hsu <smile.hsu@gmail.com>
2023-12-24 00:11:06 +0800

    init 影片06 第一個qgl查詢

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "client";
+
+export default function Home(props) {
+  console.log("PROPS: ", props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            blocks(postTemplate: false)
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      blocks: data.nodeByUri.blocks,
+      myexampleprop: "test",
+    },
+  };
+};
+s;
